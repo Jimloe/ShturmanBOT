aaeca25ffc3e91783f60885d302161529ff7408e
Jimloe <40555776+Jimloe@users.noreply.github.com>
2022-01-22 16:04:09 +0100

    Delete bot directory
    Restructuring the bot due to disnake / slash commands.

--- bot/reddit_backup.py
+++
@@ -1,8 +0,0 @@
-import random
-from bot import reddit_login
-from bot import shturclass
-
-# Automod configuration location
-# https://old.reddit.com/r/EscapefromTarkov/wiki/edit/config/automoderator/
-
-
